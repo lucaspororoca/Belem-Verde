47a68e04c2535a4674e52c219bc546f064f7dd05
Lucas Pororoca <lucas.pororoca26@gmail.com>
2025-05-14 03:00:49 -0300

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,20 @@
+const { criptografarSenha } = require('./cript');
+const db = require('better-sqlite3')('ProjetoCop30.db');
+
+function validarIdiomas(idiomas) {
+    return idiomas.length <= 3;
+}
+
+function validarEmailUnico(email) {
+    const stmt = db.prepare('SELECT * FROM users WHERE email = ?');
+    const user = stmt.get(email);
+    return !user;
+}
+
+function validarUsernameUnico(username) {
+    const stmt = db.prepare('SELECT * FROM users WHERE username = ?');
+    const user = stmt.get(username);
+    return !user;
+}
+
+module.exports = { validarIdiomas, validarEmailUnico, validarUsernameUnico };
